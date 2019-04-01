335ba98bed1afb1166bdc59b4faf3ff04292b6b2
Franco Naccarato <f-nacc-21@hotmail.com>
2019-04-01 15:38:28 -0400

    Updates

---
+++ precache-manifest.816325ce95d05811e75628d773b75efb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da967fc2f794cae43857",
+    "url": "/quote-app/static/js/runtime~main.da967fc2.js"
+  },
+  {
+    "revision": "911c8b839bf15d3deae7",
+    "url": "/quote-app/static/js/main.911c8b83.chunk.js"
+  },
+  {
+    "revision": "8b6de34057951420861c",
+    "url": "/quote-app/static/js/1.8b6de340.chunk.js"
+  },
+  {
+    "revision": "911c8b839bf15d3deae7",
+    "url": "/quote-app/static/css/main.7df4f279.chunk.css"
+  },
+  {
+    "revision": "8651b6e482e287e8397c4ad6dc1c0f5b",
+    "url": "/quote-app/index.html"
+  }
+];
